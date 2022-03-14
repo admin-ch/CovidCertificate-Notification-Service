7ab6aa155becc64cff4b122d4d30ad2e7d7c995b
Nino Di Natale <nino.dinatale@bit.admin.ch>
2022-03-14 09:04:50 +0100

    changed @NotEmpty to @NotBlank

--- src/main/java/ch/admin/bag/covidcertificate/api/request/MessageDto.java
+++ src/main/java/ch/admin/bag/covidcertificate/api/request/MessageDto.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 
 import static ch.admin.bag.covidcertificate.api.Constants.DE_MUST_NOT_BE_EMPTY;
 import static ch.admin.bag.covidcertificate.api.Constants.EN_MUST_NOT_BE_EMPTY;
@@ -15,12 +15,12 @@ import static ch.admin.bag.covidcertificate.api.Constants.IT_MUST_NOT_BE_EMPTY;
 @NoArgsConstructor
 @Getter
 public class MessageDto {
-    @NotEmpty(message = DE_MUST_NOT_BE_EMPTY)
+    @NotBlank(message = DE_MUST_NOT_BE_EMPTY)
     private String de;
-    @NotEmpty(message = FR_MUST_NOT_BE_EMPTY)
+    @NotBlank(message = FR_MUST_NOT_BE_EMPTY)
     private String fr;
-    @NotEmpty(message = IT_MUST_NOT_BE_EMPTY)
+    @NotBlank(message = IT_MUST_NOT_BE_EMPTY)
     private String it;
-    @NotEmpty(message = EN_MUST_NOT_BE_EMPTY)
+    @NotBlank(message = EN_MUST_NOT_BE_EMPTY)
     private String en;
 }
